5bae3f041285f9cee3f7451331dd16d2c4c6b23c
fernando.reta <freta@finmancorp.com>
2025-05-02 13:59:24 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+ // let person = {
+//     age: 20,
+//     name: 'Diego',
+//     height: 1.75,
+//     gamesPlayed: [
+//         { name: 'Cyberpunk', launchDate: 2020, isFinished: true },
+//         { name: 'GTA', launchDate: 2017, isFinished: false },
+//     ],
+//     hair: {
+//         color: 'black',
+//         long: false
+//     }
+// };
+// console.log(person);
+
+// let concat = 'fer ' + 'tiene ' + 35;
+// console.log(concat);
+let myString = 'string';
+const myNumber = 10;
+const myObj = {
+    color: 'white',
+    name: 'example'
+};
+myString = ' other';
+myObj.checklist = 'checklist';
+///////
+console.log(myObj);
