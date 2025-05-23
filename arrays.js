3290c54eedab724947122bdb7d4c3e0eb2dad065
fernando.reta <freta@finmancorp.com>
2025-05-23 15:04:54 -0500

    arrays file

---
+++ arrays.js
@@ -0,0 +1,19 @@
+const movie = {
+    personajePrincipal: "Luffy",
+    personajeSecundario: "Zoro",
+    name: 'name movie',// string
+    alreadyWatched: false,// boolean
+    seasons: 2, //number
+    villanos: 'crododile',
+    personajesTerciarios: ['usop', 'sanji', 'chopper'],// String array
+    personajeConFrutaDelDiablo: true,
+    numeroDeCapitulosQueapAparecioElPersonaje: 200 
+};
+
+// string -> 'Cadena de Texto'
+// number -> 0 .... 12312341 | sin comillas
+// boolean -> true | false -> sin comillas
+// objects -> object = { prop: 'val }
+// arrays -> []
+
+console.log(movie.personajesTerciarios[0]);
